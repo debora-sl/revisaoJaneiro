741c2b387fb81cc4de9e3b33b1f06a1cf6cac9bc
Debora Souza <debora.souzalima22@gmail.com>
2025-03-07 21:41:16 -0300

    Iniciando as funções e criação do prefix do Jogo da Velha.

--- backend/app/Http/Controllers/JogoDaVelhaController.php
+++ backend/app/Http/Controllers/JogoDaVelhaController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\JogoDaVelha;
 use Illuminate\Http\Request;
 
 class JogoDaVelhaController extends Controller
 {
-    //
+    // função inserir
+
+    // função carregar as posições
+
 }
