0418f7aecdc797791db5b9439ffca013b9d85266
Debora Souza <debora.souzalima22@gmail.com>
2025-03-11 08:53:42 -0300

    Criando a função de inserir.

--- backend/app/Http/Controllers/JogoDaVelhaController.php
+++ backend/app/Http/Controllers/JogoDaVelhaController.php
@@ -8,6 +8,20 @@ use Illuminate\Http\Request;
 class JogoDaVelhaController extends Controller
 {
     // função inserir
+    public function inserir(Request $request)
+    {
+        // checando se a jogada é do mesmo jogador no jogo
+        $ultimaJogada = JogoDaVelha::where('jogador', $request->jogador)->where('idJogo', $request->idJogo)->count();
+        if ($ultimaJogada > 0)
+            return response('Ops: ', 422);
+
+        $jogoDaVelha = new JogoDaVelha();
+        $jogoDaVelha->jogador = $request->jogador;
+        $jogoDaVelha->posicao = $request->posicao;
+        $jogoDaVelha->idJogo = $request->idJogo;
+
+        $jogoDaVelha->save();
+    }
 
     // função carregar as posições
 
