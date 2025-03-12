e50f7a1cf98b409325403ebf89cc0f9051dc88cb
Debora Souza <debora.souzalima22@gmail.com>
2025-03-12 06:31:29 -0300

    Incluindo no html a sessão do jogo e incluindo a matriz das posições. No controller pegando o idJogo e jogador através do  e incluindo a variavel matriz.

--- frontend/app/controllers/JogoDaVelhaController.js
+++ frontend/app/controllers/JogoDaVelhaController.js
@@ -1,5 +1,19 @@
 angular.module('meuApp')
-    .controller('JogoDaVelhaController', function ($scope, $http, $state) {
+    .controller('JogoDaVelhaController', function ($scope, $stateParams) {
         console.log('JogoDaVelhaController funcionou!');
+        // pegando a sessão e o jogador
+        $scope.idJogo = $stateParams.var1;
+        $scope.jogador = $stateParams.var2;
 
+        $scope.matriz = {
+            pos1: 'O',
+            pos2: '',
+            pos3: 'O',
+            pos4: '',
+            pos5: 'X',
+            pos6: '',
+            pos7: 'O',
+            pos8: '',
+            pos9: 'O',
+        }
     })
