bce7876fd6cc9ec9b8d0e93d2a81d9b2d93ab11a
Debora Souza <debora.souzalima22@gmail.com>
2025-03-20 16:05:56 -0300

    Inclusão das funções alinhaPosicoes e o get para pegar o jogo e o que já foi jogado.

--- frontend/app/controllers/JogoDaVelhaController.js
+++ frontend/app/controllers/JogoDaVelhaController.js
@@ -1,19 +1,64 @@
 angular.module('meuApp')
-    .controller('JogoDaVelhaController', function ($scope, $stateParams) {
+    .controller('JogoDaVelhaController', function ($scope, $stateParams, $http) {
         console.log('JogoDaVelhaController funcionou!');
         // pegando a sessão e o jogador
         $scope.idJogo = $stateParams.var1;
         $scope.jogador = $stateParams.var2;
+        $url = 'http://localhost:8000/';
+        $urlCarregarPosicoes = 'api/jogoDaVelha/carregarPosicoes/';
+
 
         $scope.matriz = {
             pos1: '',
             pos2: '',
-            pos3: 'O',
+            pos3: '',
             pos4: '',
-            pos5: 'X',
+            pos5: '',
             pos6: '',
             pos7: '',
             pos8: '',
             pos9: '',
         }
+
+        // alinhando as posições de acordo com o bd
+        alinhaPosicoes = function (data) {
+            for (i = 0; i < data.length; i++) {
+                if (data[i].posicao == 1) {
+                    $scope.matriz.pos1 = data[i].jogador;
+                } else if (data[i].posicao == 2) {
+                    $scope.matriz.pos2 = data[i].jogador;
+                } else if (data[i].posicao == 3) {
+                    $scope.matriz.pos3 = data[i].jogador;
+                } else if (data[i].posicao == 4) {
+                    $scope.matriz.pos4 = data[i].jogador;
+                } else if (data[i].posicao == 5) {
+                    $scope.matriz.pos5 = data[i].jogador;
+                } else if (data[i].posicao == 6) {
+                    $scope.matriz.pos6 = data[i].jogador;
+                } else if (data[i].posicao == 7) {
+                    $scope.matriz.pos7 = data[i].jogador;
+                } else if (data[i].posicao == 8) {
+                    $scope.matriz.pos8 = data[i].jogador;
+                } else if (data[i].posicao == 9) {
+                    $scope.matriz.pos9 = data[i].jogador;
+                }
+            }
+        }
+
+        // pegando o jogo
+        $http.get($url + $urlCarregarPosicoes + $scope.idJogo).then(function (response) {
+            console.log('Funcionou', response);
+            if (response.status == 200) {
+                console.log('Antes da função');
+
+                alinhaPosicoes(response.data);
+
+                console.log('Depois da Função');
+
+            }
+
+        }, function (error) {
+            console.log(error);
+
+        });
     })
